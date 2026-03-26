2086bb80041c73f5e98faaef09d9d461260f243c
cassaicus <cassaicus+git@gmail.com>
2026-03-26 19:58:12 +0900

    Initial Commit

---
+++ ndlocr-lite-mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ndlocr-lite-mac
+//
+//  Created by ibis on 2026/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ndlocr-lite-mac/ndlocr_lite_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  ndlocr_lite_macApp.swift
+//  ndlocr-lite-mac
+//
+//  Created by ibis on 2026/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ndlocr_lite_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
